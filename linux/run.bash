3d8544b1a0e46fafb656a58998fac8f837f880e8
totto2727 <kaihatu.totto2727@gmail.com>
2023-02-26 21:37:54 +0900

    CPUコア数の設定が漏れていたので修正

--- linux/run.bash
+++ linux/run.bash
@@ -4,7 +4,7 @@
 
 mkdir -p "$MACHINE"
 
-qemu-system-"$ARCH" -vga std -m "$MEMORY" -name "$NAME" \
+qemu-system-"$ARCH" -vga std -smp $CPU -m "$MEMORY" -name "$NAME" \
 	-drive file="$MACHINE/$QCOW2",if=virtio,format=qcow2 \
 	-netdev user,id=mynet0,hostfwd=tcp::7722-:22,hostfwd=tcp::7780-:80 \
 	-device e1000,netdev=mynet0
